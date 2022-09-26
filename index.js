f2ba56244a2bf4dc54c741b399be001b09ab1196
Matthew <matthew_mcaleer@hotmail.com>
2022-09-26 20:31:52 +1000

    refactored

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function getImage() {
         .then(data => {
             console.log(data[0])
             imageContainer.innerHTML = `<img id="random-image" src=${data[0].url}>
-                                <div id="modal">
-                                    <h3>${data[0].title}</h3>
-                                    <p>${data[0].explanation}</p>
-                                </div>`
+                                        <div id="modal">
+                                            <h3>${data[0].title}</h3>
+                                            <p>${data[0].explanation}</p>
+                                        </div>`
         })
     const button = document.getElementById('like-button')
     button.classList.remove('clicked-like-button')
@@ -22,23 +22,53 @@ function getImage() {
 }
 
 document.addEventListener('DOMContentLoaded', getImage)
+randomiseButton.addEventListener('click', getImage)
+likeButton.addEventListener('click', likedImage)
+//likeButton.addEventListener('click', changeLikeButtonColor)
 
 function likedImage() {
     const img = document.getElementById('random-image')
     const cloned = img.cloneNode(true)
     const likedImgDestination = document.getElementById('faved-img-div')
     likedImgDestination.appendChild(cloned)
+
+    const button = document.getElementById('like-button')
+
+    console.log(button.classList)
+
+    function removeUnlikedImage() {
+        return likedImgDestination.removeChild(likedImgDestination.lastChild)
+    }
+
+    if (button.classList.contains('clicked-like-button')) {
+        button.innerHTML = 'LIKE'
+        for (let i = 0; i < 2; i++) {
+            removeUnlikedImage();
+        }
+    } 
+    else {
+        button.innerHTML = "LIKED"
+    }
+
+    button.classList.toggle('clicked-like-button')
 }
 
+/*
 function changeLikeButtonColor() {
     const button = document.getElementById('like-button')
-    button.classList.toggle('clicked-like-button')
-    button.innerHTML = 'LIKED'
-}
 
-randomiseButton.addEventListener('click', getImage)
-likeButton.addEventListener('click', likedImage)
-likeButton.addEventListener('click', changeLikeButtonColor)
+    //button.innerHTML = 'LIKED'
+    console.log(button.classList)
+
+    if (button.classList.contains('clicked-like-button')) {
+        button.innerHTML = 'LIKE'
+    } 
+    else {
+        button.innerHTML = "LIKED"
+    }
+
+    button.classList.toggle('clicked-like-button')
+}*/
 
 imageContainer.addEventListener('mouseover', () => {
     let modal = document.getElementById('modal');
